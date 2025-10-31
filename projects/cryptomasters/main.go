ae8f558980d3a7d830c23a76ab62e4ac21423bf3
Umang Hirani <umanghirani.exe@gmail.com>
2025-10-31 18:47:43 +0530

    refactored the code to run in go routines

--- projects/cryptomasters/main.go
+++ projects/cryptomasters/main.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"frontendmasters.com/go/crypto/api"
 )
 
-func main() {
-	rate, err := api.GetRate("BTC") // the rate here is a pointer
+func getCurrencyData(currency string) {
+
+	rate, err := api.GetRate(currency) // the rate here is a pointer
 	//fmt.Println(rate.Price, err)    // the . here acts as ->
+
 	if err == nil {
 		fmt.Printf("The rate for %v is %.2f dollars \n", rate.Currency, rate.Price)
 	}
+
+}
+func main() {
+
+	var wg sync.WaitGroup // wg == WaitGroup
+	currencies := []string{"BTC", "ETH", "BCH"}
+
+	for _, currency := range currencies {
+		wg.Go(
+			func() {
+
+				getCurrencyData(currency)
+
+			})
+	}
+
+	wg.Wait()
 
 }
