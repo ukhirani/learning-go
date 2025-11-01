877c71744817c68470e7f72885418141805fbab2
Umang Hirani <umanghirani.exe@gmail.com>
2025-11-01 13:04:15 +0530

    writing basic server to write on /hello

---
+++ projects/fem-museum/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+
+	w.Write([]byte("Hello from a Go program !"))
+
+}
+func main() {
+
+	server := http.NewServeMux()
+
+	server.HandleFunc("/hello", handleHello)
+
+	err := http.ListenAndServe(":3333", server)
+
+	if err != nil {
+		fmt.Println("Error while opening the server")
+	}
+}
