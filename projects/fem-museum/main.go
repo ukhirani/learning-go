8123e4ca83e093dbba8de2859d9d7c9cd4eb0c13
Umang Hirani <umanghirani.exe@gmail.com>
2025-11-01 19:50:05 +0530

    defining the routes for the GET and POST api's

--- projects/fem-museum/main.go
+++ projects/fem-museum/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+
+	"frontendmasters.com/go/museum/api"
+	"frontendmasters.com/go/museum/data"
 )
 
 func handleHello(w http.ResponseWriter, r *http.Request) {
@@ -10,15 +14,31 @@ func handleHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from a Go program !"))
 
 }
+
+func handleTemplate(w http.ResponseWriter, r *http.Request) {
+	html, err := template.ParseFiles("templates/index.tmpl")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+	}
+	html.Execute(w, data.GetAll())
+}
+
 func main() {
+
+	fs := http.FileServer(http.Dir("./public"))
 
 	server := http.NewServeMux()
 
 	server.HandleFunc("/hello", handleHello)
+	server.HandleFunc("/template", handleTemplate)
+	server.HandleFunc("/api/exhibitions", api.Get)
+	server.HandleFunc("/api/exhibitions/new", api.Post)
+	server.Handle("/", fs)
 
-	err := http.ListenAndServe(":3333", server)
+	err := http.ListenAndServe(":3339", server)
 
 	if err != nil {
-		fmt.Println("Error while opening the server")
+		fmt.Printf("Error while opening the server %v /n", err)
 	}
 }
